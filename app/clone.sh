9fec5e140f9165fe188ce6f94c5a69b1d412168a
RamaTor <84365205+RamaTor@users.noreply.github.com>
2024-04-02 11:23:20 -0300

    Update clone.sh

--- app/clone.sh
+++ app/clone.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 RAMA=$1
 REPOSITORIO=$2
-mkdir -p /opt/prueba-framework
-
-#Clona repositorio git
-git clone -b $RAMA $REPOSITORIO /opt/prueba-framework
-chmod 755 /opt/prueba
-cd /opt/prueba/
+rm -rf /opt/framework || true
+mkdir -p /opt/framework
+git clone -b $RAMA $REPOSITORIO /opt/framework
