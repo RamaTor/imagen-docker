f452b596d532b6a3ec3f169b37472b233c152055
RamaTor <84365205+RamaTor@users.noreply.github.com>
2024-04-02 11:25:14 -0300

    Update entrypoint.sh

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-git clone -b main $REPO_DOCKER $WORKSPACE
-docker buildx build -t $NOMBRE_APELLIDO:$TAG_IMAGEN .
-chmod +x $WORKSPACE/app/*
-sh clone.sh $RAMA $REPOSITORIO
-docker run -e RAMA=$RAMA -e REPOSITORIO=$REPOSITORIO -e TAG=$TAG -e NAV=$NAV $NOMBRE_APELLIDO:$TAG_IMAGEN
-
-CI=true dive $NOMBRE_APELLIDO:$TAG_IMAGEN > log.txt
-cat log.txt
-
-docker run --rm -i hadolint/hadolint:latest < Dockerfile > logfile.txt || true
-cat logfile.txt
-
-trivy image $NOMBRE_APELLIDO:$TAG_IMAGEN > trivy.txt
-cat trivy.txt
-docker rmi -f $NOMBRE_APELLIDO:$TAG_IMAGEN
-docker rmi -f hadolint/hadolint
+RAMA=$1
+REPOSITORIO=$2
+TAG=$3
+NAV=$4
+cd /opt
+chmod +x clone.sh
+chmod +x testgradle.sh
+sh clone.sh ${RAMA} ${REPOSITORIO}
+cd /opt/framework
+cp /opt/testgradle.sh /opt/framework
+sh testgradle.sh ${TAG} ${NAV}
