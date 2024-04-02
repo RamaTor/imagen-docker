5612e9c219120176a147102ef2e3e4511f57c385
RamaTor <84365205+RamaTor@users.noreply.github.com>
2024-04-02 11:22:53 -0300

    Update testgradle.sh

--- app/testgradle.sh
+++ app/testgradle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 TAG=$1
 NAV=$2
-cd /opt/prueba
-./gradlew test -Dtag=$TAG -Dnav=$NAV
+xvfb-run -a --server-args="-screen 0 1920x1080x24" gradle runWithCucumber -Ptags=$TAG -Pnav=$NAV || true
+ls /opt/framework/results
